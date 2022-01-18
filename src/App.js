72a450c42f835171cdabc21b2599f1dbec3a55d1
Akash Dwivedi <91199423+akashdwivedi6261@users.noreply.github.com>
2022-01-18 09:42:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Add from "./Components/Counter";
+import Show from "./Components/DoubleCounter";
+export default function App() {
+  return (
+    <div className="App">
+      <Add />
+      <Show />
+    </div>
+  );
+}
